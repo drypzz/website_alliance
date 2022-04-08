ecea960fe8be04f68961967c636cde143705ba55
elseif <fdfjonfmd@gmail.com>
2022-04-08 14:08:51 -0300

    att geral

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -17,7 +17,13 @@ const consts = {
     },
 };
 
+// team
 
+var devs = ['Nome', 'Nome',];
+var executivo = ['Nome', 'Nome',];
+var adm = ['Nome', 'Nome',];
+var eventos = ['Nome', 'Nome',];
+var midia = ['Nome', 'Nome',];
 
 // call function's
 
@@ -42,6 +48,43 @@ consts['navbar']['menu'].addEventListener('click',
         consts['navbar']['links'].classList.toggle('active')
     }
 );
+
+// for
+var i;
+for(i = 0; i <= devs.length; i++){
+    genereteInsert(devs[i], 'desenvolvedores');
+};
+for(i = 0; i <= executivo.length; i++){
+    genereteInsert(executivo[i], 'executivo');
+};
+for(i = 0; i <= adm.length; i++){
+    genereteInsert(adm[i], 'gestao_adm');
+};
+for(i = 0; i <= eventos.length; i++){
+    genereteInsert(eventos[i], 'eventos');
+};
+for(i = 0; i <= midia.length; i++){
+    genereteInsert(midia[i], 'midia');
+};
+
+// function generate
+
+function genereteInsert(name, type){
+    var name = ((name === '') ? false : name);
+    var type = ((type === '') ? false : type);
+
+    if (name && type){
+        document.querySelector('#'+ type + '').insertAdjacentHTML(
+            'beforeend',
+            `
+            <li>
+                <img src='assets/gfx/profile.png' alt='' />
+                <p>ALLIANCE - `+ name +`</p>
+            </li>
+            `
+        );
+    };
+};
 
 
 
